8ac8f078bca589fec43b9f05e8b3305a699b338a
StepanetsRoman <vtk241_srs@student.ztu.edu.ua>
2026-06-25 15:48:54 +0300

    Delete src directory

--- src/utils/getTypeTextColor.js
+++
@@ -1,9 +0,0 @@
-export function getTypeTextColor(hexColor) {
-  const hex = hexColor.replace('#', '');
-  const red = parseInt(hex.substring(0, 2), 16);
-  const green = parseInt(hex.substring(2, 4), 16);
-  const blue = parseInt(hex.substring(4, 6), 16);
-  const luminance = (0.299 * red + 0.587 * green + 0.114 * blue) / 255;
-
-  return luminance > 0.6 ? '#1a1a2e' : '#ffffff';
-}
